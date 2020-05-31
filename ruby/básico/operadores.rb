06269fc77b45e43deab5f0b241cb6adec46de14b
IsabelaOliveira272018 <isabela.barros@compasso.com.br>
2020-05-31 16:54:07 -0300

    operadores lógicos

---
+++ ruby/básico/operadores.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# Operações matemáticas
+
+# numero1 = 0
+
+# numero2 = 0
+
+# total = 0
+
+# puts 'informe uo numero 1: '
+
+# numero1 = gets.chomp.to_i
+
+# puts 'informe o numero 2: '
+
+# numero2 = gets.chomp.to_i
+
+# total = numero1 + numero2
+
+# total = numero1 - numero2
+
+# total = numero1 * numero2
+
+# total = numero1 / numero2
+
+# puts total
+
+# Operadores de comparação
+
+v1 = 12
+
+v2 = 11
+
+# puts v1 != v2
+
+# maior que >
+# menor que <
+# maior ou igual >
+# menor ou igual <
+# diferente !=
+# igual a ==
+# igual equal?
+
+puts v1.equal? v2
