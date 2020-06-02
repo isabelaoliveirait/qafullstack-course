2427756cbdcb97fc4cde07ff06a8eb47c796eeeb
IsabelaOliveira272018 <isabela.barros@compasso.com.br>
2020-06-02 18:21:18 -0300

    tipos primitivos

---
+++ ruby/básico/tipos.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# -- Tipos em Ruby -- Tipagem dinâmica
+
+# o tipo de uma variavel em ruby é definido no momento em que a mesma recebe um valor
+
+produto = 'Macbook Air'
+
+preco = 5999.99
+
+quantidade = 10
+
+disponivel = true
+
+puts produto
+
+puts preco
+
+puts quantidade
+
+puts disponivel
+
+quantidade = '9'
+
+puts produto.class
+
+puts preco.class
+
+puts quantidade.class
+
+puts disponivel.class
