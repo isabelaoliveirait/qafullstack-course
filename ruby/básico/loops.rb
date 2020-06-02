f699414b182eeb95ff86ddf6db445d4c596d86b4
IsabelaOliveira272018 <isabela.barros@compasso.com.br>
2020-06-02 18:22:48 -0300

    looping

---
+++ ruby/básico/loops.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+
+
+# estrutura de repetição
+
+
+
+# 10.times
+
+
+
+#   puts 'Repetindo a mensagem'
+
+
+
+# end
+
+
+
+ #arrays
+
+
+
+ 5.times do |i|
+
+
+
+puts 'Repetindo a mensagem' + i.to_s + 'vez(es)'
+
+
+
+end
+
+
+
+init = 0
+
+
+
+while init <= 600
+
+
+
+puts 'Sequência Binária: ' + init.to_s
+
+
+
+# incrementando +1 ao valor de init a cada execução
+
+
+
+init += 1
+
+
+
+end
+
+
+
+ for itens in (0...10)
+
+
+
+     puts 'Repetindo a mensagem' + itens.to_s + 'vez(es)'
+
+
+
+# end
+
+
+
+# Arrays
+
+
+
+#vingadores = ['Ironman, Hulk, SpiderMan, Thor']
+
+
+
+# imprime o tipo da variavel ou objeto o .class
+
+
+
+# puts vingadores.class
+
+
+
+# vingadores.each do |v|
+
+#   puts v
+
+# end
+
