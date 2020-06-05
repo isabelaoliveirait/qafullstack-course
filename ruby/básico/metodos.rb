b034aa6a18eec66837f121e955c36f182cc54785
IsabelaOliveira272018 <isabela.barros@compasso.com.br>
2020-06-05 11:29:02 -0300

    metodos em ruby

---
+++ ruby/básico/metodos.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+
+
+def diga_ola(nome)
+
+  puts 'olá,' + nome
+
+end
+
+
+
+diga_ola('Isabela')
+
+
+
+def retorna_nome
+
+  'Fernando'
+
+end
+
+
+
+resultado = retorna_nome
+
+
+
+puts resultado
+
+
+
+# def soma(v1, v2)
+
+
+
+#  total = v1 + v2
+
+
+
+#  total
+
+
+
+# end
+
+
+
+# res = soma(10, 25)
+
