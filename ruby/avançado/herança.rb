3ffa241b495b67df46ba920057f3748567e047d1
IsabelaOliveira272018 <isabela.barros@compasso.com.br>
2020-06-07 00:19:04 -0300

    herança em ruby

--- ruby/avançado/herança.rb
+++ ruby/avançado/herança.rb
@@ -1,5 +1,4 @@
-class Carro
-
+class Veiculo #super classe
     attr_accessor :nome, :marca, :modelo #definindo atributos
 
     def initialize(nome, marca, modelo)
@@ -18,16 +17,35 @@ class Carro
     def buzinar
         puts 'Beep! Beep!'
     end
+end
+#classe filha
+class Carro < Veiculo # para herdar funções/recursos da classe Veiculo é só usar "<"
 
     def dirigir
         puts "#{nome} iniciando o trajeto."
     end
 end
 
+#classe filha
+class Moto < Veiculo
+    
+    def pilotar
+        puts "#{nome} iniciando o trajeto."
+    end
+
+end
+
 civic = Carro.new('Civic', 'Honda', 'SI') #uma nova instancia e passa os valores dentro do argumento
 puts civic.nome
 civic.ligar
+civic.dirigir
 
 lancer = Carro.new('Lancer', 'Mit', 'EVO')
 lancer.ligar
 lancer.buzinar
+lancer.dirigir
+
+fazer = Moto.new('Fazer', 'Yamaha', '250x')
+fazer.ligar
+fazer.buzinar
+fazer.pilotar
