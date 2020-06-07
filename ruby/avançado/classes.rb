49d8c0a124e2e630ba716eb69f3cf3939a856e8f
IsabelaOliveira272018 <isabela.barros@compasso.com.br>
2020-06-06 22:08:09 -0300

    trabalhando com construtores

--- ruby/avançado/classes.rb
+++ ruby/avançado/classes.rb
@@ -5,18 +5,26 @@
 #caracteristicas e ações
 
 class Conta
-  attr_accessor :saldo
+  attr_accessor :saldo, :nome #atributos
 
+  def initialize(nome) #initialize é o construtor em ruby
+    self.saldo = 0.0
+    self.nome = nome
+  end
+  
   def deposita(valor)
    # puts 'depositando a quantia de: ' + valor.to_s + 'reais'
-
     # self é um recurso do ruby para invocar os atributos dentro da propria classe, é como se fosse o"this" do java
     self.saldo += valor
-    
-    puts "depositando a quantia de #{valor} reais" # interpolação de string pra não precisar converter variavel valor pra string
+    puts "depositando a quantia de #{valor} reais na conta do #{self.nome}." # interpolação de string pra não precisar converter variavel valor pra string
   end
 end
-c = Conta.new #instanciando uma nova conta na variavel C
-c.saldo = 0.0 #o saldo da conta começa com zero
+c = Conta.new('Teste') #instanciando uma nova conta na variavel C // atribuindo um argumento chamado de Teste
+
+#c.saldo = 0.0 o saldo da conta começa com zero
 c.deposita(100.00)
 puts c.saldo
+
+c.deposita(50.00)
+p c.saldo
+p c.nome
