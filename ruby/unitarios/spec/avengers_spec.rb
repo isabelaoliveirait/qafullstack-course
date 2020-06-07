f0011faa349151d018839f93273796e8125458e9
IsabelaOliveira272018 <isabela.barros@compasso.com.br>
2020-06-07 18:48:16 -0300

    verificadores e validadores do Rspec

--- ruby/unitarios/spec/avengers_spec.rb
+++ ruby/unitarios/spec/avengers_spec.rb
@@ -19,4 +19,43 @@ describe AvengersHeadQuarter do
         hq.put('Spiderman')
         expect(hq.list).to eql ['Spiderman']
     end
+
+    it 'deve adicionar uma lista de vingadores' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Ich')
+
+       resultado = hq.list.size > 0 #retorna um booleano se é true ou false
+
+       expect(hq.list).to include 'Hulk' #verificador include pra validar se o registro contém na lista
+       expect(hq.list.size).to be > 0 #verifica o tamanho da lista pra saber se é realmente uma lista e contém elementos
+    end
+
+    it 'thor deve ser o primeiro da lista' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Ich')
+
+        expect(hq.list).to start_with('Thor')
+    end
+
+    it 'Ironman deve ser o ultimo da lista' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Ich')
+        hq.put('Ironman')
+
+        expect(hq.list).to end_with('Ironman')
+    end
+
+    it 'deve conter o sobrenome' do
+
+        avenger = 'Peter Parker'
+        expect(avenger).to match(/Parker/) #utilizando expressoes regulares
+        expect(avenger).not_to match(/isa/) #validar que não possui 
+    end
+
 end
