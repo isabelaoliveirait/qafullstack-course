50599cb3b9b9d17bf820073d142241a2ec27c0e5
IsabelaOliveira272018 <isabela.barros@compasso.com.br>
2020-06-07 18:17:25 -0300

    Gerando testes unitários com rspec orientado a TDD

---
+++ ruby/unitarios/spec/exemplos_spec.rb
@@ -0,0 +1,10 @@
+describe 'suite rspec' do
+
+    it 'some de valores' do
+        soma = 10 + 5
+        expect(soma).to eql 15 #convertendo o valor da soma pra String basta colocar do lado da soma o ".to_s"
+
+    end
+
+
+end
