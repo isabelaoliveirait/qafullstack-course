cd67fe43d573c19e2c0d589c76830d5ba2231e61
IsabelaOliveira272018 <isabela.barros@compasso.com.br>
2020-06-07 23:01:56 -0300

    trabalhando com import relativo

--- ruby/unitarios/spec/saque_spec.rb
+++ ruby/unitarios/spec/saque_spec.rb
@@ -1,24 +1,4 @@
-class ContaCorrente
-
-    attr_accessor :saldo, :mensagem
-
-    def initialize(saldo)
-        self.saldo = saldo
-    end
-
-    def saca(valor) 
-        if (valor > self.saldo)
-            self.mensagem = 'Saldo insuficiente para saque'
-
-        elsif (valor > 700.00)
-            self.mensagem = 'Limite máximo por saque é de R$ 700 reais'
-
-        else
-            self.saldo -= valor
-
-        end
-    end
-end
+require_relative '../app/bank' #para importar a classe de forma relativa utilizando o caminho
 
 describe ContaCorrente do
     describe 'Saque' do
